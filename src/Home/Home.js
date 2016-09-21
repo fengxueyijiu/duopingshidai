f84708bb5a183bbb1392be2733178e5d8da0c73a
Your Name <you@example.com>
2016-09-21 22:07:08 +0800

    case

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,12 +2,21 @@ import React from 'react'
 
 class Home extends React.Component {
   render () {
+    let styles={
+      h2:{
+        fontSize:"42px",
+        fontWeight:"700",
+        marginBttom:"20px"
+      }
+    }
     return(
       <div className="home-container">
         <div className="home-cover">
-          <h3>I'm newming</h3>
-          <p> WEB DEVEL OPER</p>
-          <button>HIRE ME</button>
+          <div className="home-item">
+            <h2 style={styles.h2}>HI,I'M<span style={{color:"#00bcd4"}}>WU JIANBO</span></h2>
+            <p style={{marginBttom:"20px"}}> WEB DEVEL OPER</p>
+            <button className="home-btn" > <a href="https://github.com/fengxueyijiu" style={{color:"#fff"}}>HIRE ME</a></button>
+          </div>
         </div>
 
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './main.css';
 import Routers from './routes';
