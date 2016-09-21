8761d03defbe3aac1c96e267e91353ff4d4d3705
Your Name <you@example.com>
2016-09-21 14:16:59 +0800

    buzhi

---
+++ src/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Home extends React.Component {
+  render () {
+    return(
+      <div className="home-container">
+        <div className="home-cover">
+          <h3>I'm newming</h3>
+          <p> WEB DEVEL OPER</p>
+          <button>HIRE ME</button>
+        </div>
+
+
+      </div>
+    )
+  }
+}
+
+export default Home;
